e30a291bc4e2e6e238d05eb73ebef21a7a6803b6
loony2m <103135265+loony2m@users.noreply.github.com>
2024-05-16 19:23:29 +0300

    Update Brand.java

--- src/main/java/com/example/dlc/models/Brand.java
+++ src/main/java/com/example/dlc/models/Brand.java
@@ -10,25 +10,19 @@ import javax.persistence.Id;
 @Entity
 public class Brand {
     @Setter
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Setter
+    @Getter
     private String name;
     @Setter
+    @Getter
     private double averageRating;
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getAverageRating() {
-        return averageRating;
-    }
+    @Setter
+    @Getter
+    @OneToMany(mappedBy = "brand", cascade = CascadeType.ALL)
+    private List<Product> products;
 }
-
-
