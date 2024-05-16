5245965e6f2cc7e8c5505a46a17b59fe67cd5a57
loony2m <103135265+loony2m@users.noreply.github.com>
2024-05-16 19:30:56 +0300

    Update Manufacturer.java

--- src/main/java/com/example/dlc/models/Manufacturer.java
+++ src/main/java/com/example/dlc/models/Manufacturer.java
@@ -1,11 +1,10 @@
 package com.example.dlc.models;
 
+import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Manufacturer {
