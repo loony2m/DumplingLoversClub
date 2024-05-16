1b9e166b257aed9bc78ef54d11fa962bbc750aa0
loony2m <103135265+loony2m@users.noreply.github.com>
2024-05-16 19:30:40 +0300

    Update Brand.java

--- src/main/java/com/example/dlc/models/Brand.java
+++ src/main/java/com/example/dlc/models/Brand.java
@@ -1,11 +1,10 @@
 package com.example.dlc.models;
 
+import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Brand {
@@ -26,3 +25,5 @@ public class Brand {
     @OneToMany(mappedBy = "brand", cascade = CascadeType.ALL)
     private List<Product> products;
 }
+
+
