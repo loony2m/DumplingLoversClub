de798b4072dedddb118c9b349f48798f17078d3f
loony2m <103135265+loony2m@users.noreply.github.com>
2024-05-17 14:27:42 +0300

    Update ManufacturerService.java

--- src/main/java/com/example/dlc/services/ManufacturerService.java
+++ src/main/java/com/example/dlc/services/ManufacturerService.java
@@ -2,7 +2,6 @@ package com.example.dlc.services;
 
 import com.example.dlc.models.Manufacturer;
 import com.example.dlc.repositories.ManufacturerRepository;
-import com.example.dlc.repositories.BrandRepository;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
@@ -13,11 +12,9 @@ import java.util.Optional;
 @Service
 public class ManufacturerService {
     private final ManufacturerRepository manufacturerRepository;
-    private final BrandRepository brandRepository;
 
-    public ManufacturerService(ManufacturerRepository manufacturerRepository, BrandRepository brandRepository) {
+    public ManufacturerService(ManufacturerRepository manufacturerRepository) {
         this.manufacturerRepository = manufacturerRepository;
-        this.brandRepository = brandRepository;
     }
 
     public List<Manufacturer> getAllManufacturers() {
@@ -30,16 +27,14 @@ public class ManufacturerService {
 
     @Transactional
     public void deleteManufacturer(Long manufacturerId) {
-    Manufacturer manufacturer = manufacturerRepository.findById(manufacturerId).orElseThrow(() -> new EntityNotFoundException("Производитель не найден с id: " + manufacturerId));
+        Manufacturer manufacturer = manufacturerRepository.findById(manufacturerId)
+                .orElseThrow(() -> new EntityNotFoundException("Производитель не найден с id: " + manufacturerId));
 
-    // Удаление всех товаров, связанных с производителем
-    manufacturer.getProducts().forEach(product -> product.setManufacturer(null));
-    manufacturer.getProducts().clear();
+        // Удаление всех товаров, связанных с производителем
+        manufacturer.getProducts().forEach(product -> product.setManufacturer(null));
+        manufacturer.getProducts().clear();
 
-    if (manufacturer.getBrands() != null && !manufacturer.getBrands().isEmpty()) {
-        manufacturer.getBrands().forEach(brand -> brandRepository.delete(brand));
-    } else {
+        // Удаление производителя
         manufacturerRepository.delete(manufacturer);
     }
-}
 }
