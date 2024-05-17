246a2d7a18961c6d167883ee27ff11756390786c
loony2m <103135265+loony2m@users.noreply.github.com>
2024-05-17 14:25:35 +0300

    Update Brand.java

--- src/main/java/com/example/dlc/models/Brand.java
+++ src/main/java/com/example/dlc/models/Brand.java
@@ -22,19 +22,6 @@ public class Brand {
 
     @Setter
     @Getter
-    @Transient
-    private List<Manufacturer> manufacturers;
-
-    @Setter
-    @Getter
     @OneToMany(mappedBy = "brand", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Product> products;
-
-    private Manufacturer manufacturer;
-
-    public Manufacturer getManufacturer() {
-    Manufacturer result = new Manufacturer();
-    manufacturerRepository.findById(result.getId()).ifPresent(result::set);
-    return result;
-    }
 }
